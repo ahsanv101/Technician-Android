50a2e6b9ab1cdb9fc3fcdc9d4464d7ea44d632eb
Ahsan <ms02743@st.habib.edu.pk>
2019-07-30 13:42:37 +0500

    uploaded code

---
+++ app/src/main/java/com/google/transporttracker/CustomOnItemSelectedListener.java
@@ -0,0 +1,18 @@
+package com.google.transporttracker;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+///A dummy class just a function can be used in activity
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+        
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+    }
+
+}
